25538ef6107f29bd2fc293dea814ea5e04c1e3b9
Bástyai Botond <64700017+botator@users.noreply.github.com>
2024-11-18 09:37:06 +0100

    Add files via upload

---
+++ berek/Dolgozok.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace berek
+{
+    internal class Dolgozok
+    {
+        public string Nev { get; set; }
+        public string Neme { get; set; }
+        public string Reszleg { get; set; }
+        public int BelepesEv { get; set; }
+        public int Ber { get; set; }
+
+        public Dolgozok(string sor)
+        {
+            string[] s = sor.Split(';');
+            Nev = s[0];
+            Neme = s[1];
+            Reszleg = s[2];
+            BelepesEv = Convert.ToInt32(s[3]);
+            Ber = Convert.ToInt32(s[4]);
+        }
+        public override string ToString()
+        {
+            return "\tNév: "+Nev+"\n\tNem: "+Neme+"\n\tBelépés: "+BelepesEv+"\n\tBér: "+Ber.ToString();
+        }
+    }
+    
+
+}
---
+++ berek/Program.cs
@@ -0,0 +1,34 @@
+﻿using berek;
+using System.Text;
+
+List<Dolgozok> list = new();
+
+StreamReader sr = new(path: "C:\\Users\\bastyai.botond\\Desktop\\berek\\berek\\bin\\Debug\\net8.0\\berek2020.txt",encoding: Encoding.UTF8);
+while (!sr.EndOfStream)
+{
+    list.Add(new Dolgozok(sr.ReadLine()));
+}
+sr.Close();
+
+Console.WriteLine($"3.Feladat: Dolgozók száma {list.Count} fő");
+var atlagber = list.Average(r => r.Ber);
+Console.WriteLine($"4.Feladat: Bérek átlaga {(atlagber/1000):0.0} eFt");
+Console.WriteLine("4.Feladat: Kérem a részleg nevét: ");
+string reszleg = Console.ReadLine()!;
+var vaneReszleg = list.Where(x => x.Reszleg == reszleg.ToLower());
+if(vaneReszleg.Count() < 1)
+{
+    Console.WriteLine("6.feladat: A megadott részleg nem létezik a cégnél!");
+}
+else
+{
+    Console.WriteLine("6.feladat: A legtöbbet kereső dolgozó a megadott részlegen");
+    var gazdagProli = vaneReszleg.MaxBy(x => x.Ber);
+    Console.WriteLine(gazdagProli.ToString());
+}
+Console.WriteLine("7.Feladat: Statisztika");
+var stat = list.GroupBy(x => x.Reszleg);
+foreach(var x in stat)
+{
+    Console.WriteLine("\t"+x.Key+" - "+x.Count());
+}
